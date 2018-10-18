b1c542419d237ca71e34f902d7188be603c32b75
Christian Rios <criosmancilla@gmail.com>
2018-10-17 22:55:26 -0500

    cambiando prueba python

--- python/apagarled.py
+++ python/apagarled.py
@@ -1,6 +1,5 @@
 import RPi.GPIO as gpio
 gpio.setmode(gpio.BOARD)
-gpio.setmode(gpio.BCM)
-gpio.setmode(gpio.BOARD)
-gpio.setup(12, gpio.OUT)
-gpio.output(12, True)
+
+gpio.setup(3, gpio.OUT)
+gpio.output(3, True)
