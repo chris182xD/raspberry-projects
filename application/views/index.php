d9cfb2ad65e3ae4d0a524576fa398ba946b23903
Christian Rios <criosmancilla@gmail.com>
2018-10-17 21:45:03 -0500

    initial commit

---
+++ application/views/index.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Controllador RaspbBerry Pi</title>
+
+</head>
+<body>
+
+<h1>Hola</h1>
+</body>
+</html>
