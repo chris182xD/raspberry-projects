c77f8f3298112f8e732340e4921298b468e40c5e
Christian Rios Mancilla <criosmancilla@gmail.com>
2018-10-30 17:22:33 -0600

    Agregando control de led

--- application/views/raspberry/monitoreo.php
+++ application/views/raspberry/monitoreo.php
@@ -8,9 +8,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Monitoring</title>
-    <link rel="stylesheet" href="/raspberry-projects/materialize/css/materialize.css">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+            
 </head>
 <body>
     <h1 class="center-align">Raspberry Monitoring</h1>
+    <div class="row">
+        <div class="col s4">
+            <h5>Switch Led</h5>
+            <div class="switch">
+                <label>
+                    Off
+                    <input type="checkbox" id="SwitchLed" onclick="SwitchLed()">
+                    <span class="lever"></span>
+                    On
+                </label>
+            </div>
+        </div>
+    <div class="row">
+    </div>
 </body>
 </html>
